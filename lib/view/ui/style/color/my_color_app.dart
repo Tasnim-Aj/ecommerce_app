3324d6b2ca97479598430af207b22f7ae5b5966f
Tasnim Aj <tasnimajaj1234@gmail.com>
2025-05-04 17:54:05 +0300

    edit color

--- lib/view/ui/style/color/my_color_app.dart
+++ lib/view/ui/style/color/my_color_app.dart
@@ -2,14 +2,6 @@ import 'package:flutter/material.dart';
 
 class ColorsApp {
   static const Color primaryColor = Color(0xFFFFA451);
-  // static const Color shadowPrimaryColor = Color(0xFF0EBE7E);
-  // static const Color shadowSecondryColor = Color(0xFF61CEFF);
-  // static const Color secondryColor = const Color(0xFFfcebe4);
   static const Color backgroundColor = Color(0xFFFFFFFF);
-
   static const Color textColor = Color(0xFF27214D);
-  // static const Color titleTextColor = Color(0xFF333333);
-  // static const Color subtitleTextColor = Color(0xFF677294);
-  // static const Color descriptionColor = Color(0xFF959CB4);
-  // static const Color rateStarColor = Color(0xFFF6D060);
 }
